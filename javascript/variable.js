2d783bd0b8de2f98c48d9592d653333793129f61
Shreya Lokhande <lokhandeshreya165@gmail.com>
2026-08-01 00:54:17 +0530

    Add variable.js

---
+++ javascript/variable.js
@@ -0,0 +1,17 @@
+const account_Id = 1234
+let account_email = "shreya@google.com"
+var account_password = "shreya123"
+account_city = "nashik"
+
+// account_Id = 5678
+console.log(account_Id);
+
+account_email = "shruti98@google.com"
+account_password = "9876"
+account_city ="pune"
+
+/*
+do not use var - issue in block and function scope
+*/
+
+console.table([account_Id,account_email,account_password,account_city])
