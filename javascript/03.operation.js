fbf7113564823244d840f4d77776b3e5037b2431
Shreya Lokhande <lokhandeshreya165@gmail.com>
2026-08-02 17:48:34 +0530

    Added 03.operation.js

---
+++ javascript/03.operation.js
@@ -0,0 +1,41 @@
+let value = 3
+let negValue = -value
+console.log(value);
+console.log(negValue);
+
+console.log(4+4);     //add
+console.log(4-2);     //sub
+console.log(4*2);     //mul
+console.log(4**2);    //square
+console.log(4/2);     //div
+console.log(4%2);     //mod-div
+
+ //* string operation *//
+ 
+let str1 = "hello"
+let str2 = " shreya"
+let str3 = str1 + str2
+console.log(str3);         // hello shreya
+
+console.log("1" + "3");  // 13
+console.log("1" + 2);    //12
+console.log("1" + 2 + 3);  // 123
+console.log(1 + 2 + "3");  //33
+console.log((3+4) * 2 / 2); //7
+
+console.log(+true);  //1
+console.log(+"");    //0
+console.log(-"true"); //NaN
+
+let num1, num2, num3
+num1 = num2 = num3 = 9
+console.log(num2);   // 9
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); //101
+
+let gameCounter = 100
+--gameCounter;
+console.log(gameCounter); //99
