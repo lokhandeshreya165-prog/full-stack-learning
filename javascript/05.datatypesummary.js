80b62f550430d013f3c34cee17abe687bb9c99c1
Shreya Lokhande <lokhandeshreya165@gmail.com>
2026-08-04 00:09:12 +0530

    Added 05.datatypesummary.js

--- javascript/05.datatypesummary.js
+++ javascript/05.datatypesummary.js
@@ -8,17 +8,29 @@ Undefined : declare memory space
 Symbol : used for make unique
 BigInt : .............n
 */ 
-// const score = 100
-// const scoreValue = 100.3
-// const isLoggedIn = false
-// const outsideTemp = null
-// let userName;
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userName;
 const Id = Symbol('567')
 const SId = Symbol('567')
 console.log( Id = SId);
 
-// const a = 357436939479n
+const a = 357436939479n
+
 /*
 Reference (Non primitive)
 Array , Object, Function 
 */
+const heros = ["shreya" , "shruti", " satyam"]
+let myobj = {
+    name : "shourya",
+    age: 4,
+}
+
+//declaratio of function
+const myfun = function(){
+    console.log("shreya");
+}
