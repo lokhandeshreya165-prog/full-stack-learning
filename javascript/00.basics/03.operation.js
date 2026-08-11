fe4794f31d2bee82bce6351ca8c96b9141055c2c
Shreya Lokhande <lokhandeshreya165@gmail.com>
2026-08-12 01:56:47 +0530

    Added 00.basics

---
+++ javascript/00.basics/03.operation.js
@@ -0,0 +1,42 @@
+let value = 3
+let negValue = -value
+console.log(value);  //3
+console.log(negValue);  // -3
+
+console.log(4+4);     //add
+console.log(4-2);     //sub
+console.log(4*2);     //mul
+console.log(4**2);    //square
+console.log(4/2);     //div
+console.log(4%2);     //mod-div
+
+ //* string operation *//
+
+let str1 = "hello"
+let str2 = " shreya"
+let str3 = " Lokhande"
+let str4 = str1 + str2 + str3
+console.log(str4);         // hello shreya Lokhande
+
+console.log("1" + "3");  // 13
+console.log("1" + 2);    //12
+console.log("1" + 2 + 3);  // 123
+console.log(1 + 2 + "3");  //33
+console.log((3+4) * 2 / 2); //7
+
+console.log(+true);  //1
+console.log(+"");    //0
+console.log(-"true"); //NaN
+
+let num1, num2, num3
+num1 = num2 = num3 = 9
+console.log(num2);   // 9
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); //101
+
+let gameCounter = 100
+--gameCounter;
+console.log(gameCounter); //99
---
+++ javascript/00.basics/05.datatypesummary.js
@@ -0,0 +1,55 @@
+/* primitive : 
+7 Types
+String : called by value
+Number : 
+boolean : true, false
+Null : Empty 
+Undefined : declare memory space
+Symbol : used for make unique
+BigInt : .............n
+*/ 
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userName;
+const Id = Symbol('567')
+const SId = Symbol('567')
+console.log( Id = SId);
+
+const a = 357436939479n
+
+/*
+Reference (Non primitive)
+Array , Object, Function 
+*/
+const heros = ["shreya" , "shruti", " satyam"]
+let myobj = {
+    name : "shourya",
+    age: 4,
+}
+
+//declaratio of function
+const myfun = function(){
+    console.log("shreya");
+}
+
+/***********************/
+//stack-memory = primitive datatype
+//heap-memory = non-primitive datatype
+
+let myname = "shreya"
+let anothername = myname
+anothername = "SVL"
+console.log(anothername);
+console.log(myname);
+
+let user1 = {
+    email: "shreya@gmail.com",
+    upiid:"shreya123"
+}
+let user2 = user1
+user2.email = " shrey678@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
