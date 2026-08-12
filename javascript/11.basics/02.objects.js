3988764a3de2af6116c6f8fef3cfed47e1a9fa44
Shreya Lokhande <lokhandeshreya165@gmail.com>
2026-08-12 15:58:01 +0530

    Added 02.objects.js

---
+++ javascript/11.basics/02.objects.js
@@ -0,0 +1,14 @@
+//singleton 
+Object.create    //constructor method
+
+//obj literals
+const obj = {
+    name: "shruti",
+    "full name" : "shreya lokhande",
+    age :20,
+    location : "pune",
+    email : "shreya@gmail.com"
+}
+//way1
+console.log(obj.name);  //shruti
+console.log(obj.location);   //pune
