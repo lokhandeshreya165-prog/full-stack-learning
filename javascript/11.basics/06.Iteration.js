dffeb28f79a00fe40c0d791f3c888442e66eaa32
Shreya Lokhande <lokhandeshreya165@gmail.com>
2026-08-17 18:07:21 +0530

    Added 06.Iteration.js

--- javascript/11.basics/06.Iteration.js
+++ javascript/11.basics/06.Iteration.js
@@ -34,3 +34,85 @@ do{
     console.log(`value is ${value}`);
     value++
 }while (value <=10)
+
+//array loop
+/*****1.for-of loop***/
+const arr = [1,2,3,4,,5,6]
+for (const num of arr ){
+    console.log(num);     //op: 1 2 3 4 undefined 5 6 
+}
+
+const string = "shreya"
+for(const str of string){
+    console.log(` ${str}`);   //op: s h r e y a 
+}
+
+//map -- used for unique value duplicate not allow. it write a only 2nd value when dublicate are found 
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "united states of america")
+map.set('Fr', "france")
+map.set('IN', "india")
+
+console.log(map);   ///op: Map(3) {size: 3, IN => indea, USA => united sta…f america, Fr => france}
+//duble india is not write
+for(const [key,value] of map){
+    console.log(key, ':',value);
+}  
+//op:
+// IN : india
+// USA : united states of america
+// Fr : france
+
+//only map are iterativa object is not iterate 
+//how to write object for iteration - by using for in loop
+//don'y used for-in loop on map
+/****2.for-in loop***/
+//in object
+const myobject = {
+    js : "javascript",
+    py : "python",
+    DB : "database",
+    rb : "ruby"
+}
+for(const key in myobject){
+    //console.log(key);         //op: js py DB rb 
+    console.log(`${key}  -- shortcut for '${myobject[key]}'`);
+}
+/*op:
+js  -- shortcut for 'javascript'
+py  -- shortcut for 'python'
+DB  -- shortcut for 'database'
+rb  -- shortcut for 'ruby'
+*/
+
+//for-in loop for array
+const lang = ["js","py", "db","cpp"]
+for(const key in lang){
+    //console.log(key);     //it give num key op: 0 1 2 3  
+    console.log(lang[key]);   //op: js py db cpp
+}
+
+//for-each loop  -- for array-function
+const myCoding = [
+    {
+        langname : "javascript",
+        langfile : "js"
+    },
+    {
+        langname : "python",
+        langfile : "py"
+    },
+    {
+        langname : "style",
+        langfile : "cs"
+    }
+]
+myCoding.forEach((item) => {
+    console.log(`${item.langname}, shortform is : ${item.langfile}`);
+})
+/*op:
+javascript, shortform is : js
+python, shortform is : py
+style, shortform is : cs
+*/
